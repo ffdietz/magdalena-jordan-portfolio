46751a8128c14a9e80a51a251a66444e4bd20a07
ffdietz <felipefierrod@gmail.com>
2024-01-13 04:00:13 +0100

    project details conditional fields

--- src/components/Projects/ProjectDetails.tsx
+++ src/components/Projects/ProjectDetails.tsx
@@ -9,19 +9,25 @@ const ProjectDetails = ({project}: {project: TProject}) => {
     <Stack maxW="2xl">
       {project && (
         <>
+        {title &&
           <Text>{title}</Text>
+        }
+        {location &&
           <Map
-            height={300}
-            width={700}
-            defaultZoom={16}
-            defaultCenter={[location.lat, location.lon]}
+          height={300}
+          width={700}
+          defaultZoom={16}
+          defaultCenter={[location.lat, location.lon]}
           >
             <Marker
               width={30}
               anchor={[location.lat, location.lon]}
-            />
+              />
           </Map>
+        }
+        {description &&
           <Text>{description}</Text>
+        }
         </>
       )}
     </Stack>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,13 @@
-import { Asset, Entry, EntrySkeletonType } from "contentful";
+import { Asset } from "contentful";
 
 export type TProject = {
-    title: string;
-    description: string;
-    location: TLocation;
-    images: Asset[];
+  title: string;
+  description: string;
+  location: TLocation;
+  images: Asset[];
 };
 
 export type TLocation = {
   lat: number;
   lon: number;
 };
-
-export type ProjectSkeletonType = EntrySkeletonType<TProject>;
-
-export type ProjectEntry = Entry<ProjectSkeletonType>;
