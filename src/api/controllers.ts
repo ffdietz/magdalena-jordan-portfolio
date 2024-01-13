a923131116c46b789837bf5bfb67c809aa132570
ffdietz <felipefierrod@gmail.com>
2024-01-13 03:43:30 +0100

    controller typing

--- src/api/controllers.ts
+++ src/api/controllers.ts
@@ -1,26 +1,17 @@
-import { ProjectEntry, ProjectFields} from '@types';
+import { TProject } from "@types";
 import { client } from './client'
 
-export const getProjects = async (): Promise<ProjectEntry[] | undefined> => {
-  try{
+export const getProjects = async (): Promise<TProject[] | undefined> => {
+  try {
     const response = await client.getEntries({
       content_type: import.meta.env.VITE_APP_CONTENTFUL_CONTENT_TYPE,
       order: ["fields.title"],
     });
-
-    const projects: ProjectFields[] = response.items.map((entry) => entry.fields)
     
-    return projects as ProjectEntry[];
-  } catch (error){ console.log(error) }
-}
-
-  
-  // await client
-  //   .getEntries({
-  //     content_type: import.meta.env.VITE_APP_CONTENTFUL_CONTENT_TYPE,
-  //     order: ["fields.title"],
-  //   })
-  //   .then((response) => {
-  //     return response.items;
-  //   })
-  //   .catch(console.error);
+    const projects = response.items.map((entry) => entry.fields);
+    
+    return projects as TProject[];
+  } catch (error) {
+    console.error(error);
+  }
+};
