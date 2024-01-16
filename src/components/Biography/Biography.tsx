20ed914799a494d73e176b76a297ee6c97d1e751
ffdietz <felipefierrod@gmail.com>
2024-01-16 20:20:47 +0100

    bio layout

--- src/components/Biography/Biography.tsx
+++ src/components/Biography/Biography.tsx
@@ -1,28 +1,28 @@
-import {
-  BiographyContainer,
-  BiographyImage,
-  BiographyText,
-} from "./style";
-import { Stack } from "@chakra-ui/react";
-import { sections } from "../../content";
+import { Flex, Image, Stack, Text } from "@chakra-ui/react";
+import { biography, sections } from "../../content";
 
 const Biography = (() => {
   return (
     <Stack id={sections[1].id}>
-      <BiographyContainer>
-        <BiographyImage src={""} alt="bio-image" />
+      <Flex minH="3xl" justifyContent="space-evenly" align="center" p={24}>
+        <Image
+          src=""
+          alt="bio-image"
+          boxSize="md"
+          w="xs"
+          border="1px"
+          borderColor="blue"
+        />
 
-        <BiographyText>
-          Magdalena Jordán (Santiago de Chile, 1986) es Licenciada en Artes
-          Visuales de la Universidad de Chile (2010) y Comunicadora Audiovisual
-          de I.P ARCOS (2012).
-          <br />
-          De especialidad pintora, indaga en su obra la pertinencia del lenguaje
-          narrativo en la pintura figurativa. Durante los últimos años,
-          investiga en su obra sobre la representación de la identidad, la
-          cultura y la memoria en el género del paisaje.
-        </BiographyText>
-      </BiographyContainer>
+        <Text
+          w="xl"
+          fontFamily="Andale Mono"
+          fontSize={22}
+          whiteSpace="break-spaces"
+        >
+          {biography}
+        </Text>
+      </Flex>
     </Stack>
   );
 });
