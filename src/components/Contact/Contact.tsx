8b93b9af503dc23f1e9a39d829a813b9ec03a73a
ffdietz <felipefierrod@gmail.com>
2024-01-26 01:26:12 +0100

    contact layout

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,7 +5,7 @@ import { Stack, Text } from "@chakra-ui/react";
 const Contact = (() => {
   return (
     <Stack id={sections["contact"].id} minH="3xl">
-      <Stack h="auto" alignItems="center" justifyContent="center" m="auto">
+      <Stack m="auto">
         {Object.values(contact).map((field) => (
           <Text
             key={field}
